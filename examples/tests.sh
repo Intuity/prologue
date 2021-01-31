e2a74bff7b87d2bd4145bd234513063aba05ec71
Peter Birch <peter@lightlogic.co.uk>
2021-01-31 20:15:43 +0000

    examples/tests.sh: Setting up virtual environment for testing - to work with GitHub CI

--- examples/tests.sh
+++ examples/tests.sh
@@ -17,12 +17,35 @@
 # Credit to Dave Dopson: https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Create a virtual environment
+python3 -m virtualenv ${this_dir}/venv
+source ${this_dir}/venv/bin/activate
+python3 ${this_dir}/../setup.py develop
+
 # Run all tests in subdirectories
+passed=0
+failed=0
 for test_sh in ${this_dir}/*/test.sh; do
     test_dir=$(dirname $test_sh)
     test_name=$(basename $test_dir)
     cd $test_dir
     echo "# Running test $test_name"
     ./test.sh
+    if [[ "$?" != "0" ]]; then
+        failed=$((failed + 1))
+    else
+        passed=$((passed + 1))
+    fi
     cd ${this_dir}
 done
+
+# Clean-up
+rm -rf ${this_dir}/venv
+
+# Report
+echo "# Tests Passed: $passed, Tests Failed: $failed"
+
+if (( $failed > 0 )); then
+    echo "[ERROR] Not all tests passed"
+    exit 1
+fi
